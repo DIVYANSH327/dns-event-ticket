c5af374f217dc540b32dbee0201f029e913e5436
DIVYANSH327 <143836967+DIVYANSH327@users.noreply.github.com>
2025-12-30 22:07:25 +0530

    add root route

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+app.use("/tickets", express.static("tickets"));
 
+app.get("/", (req, res) => {
+  res.send("DNS Event Backend Live ✅");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
