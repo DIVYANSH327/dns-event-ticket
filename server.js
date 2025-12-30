3b3174f754ac98afd8017b6308c3edc5dc1ecef8
DIVYANSH327 <143836967+DIVYANSH327@users.noreply.github.com>
2025-12-30 22:14:11 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+/* ROOT TEST */
+app.get("/", (req, res) => {
+  res.send("DNS Event Backend Live ✅");
+});
+
+/* CREATE ORDER */
+app.post("/create-order", async (req, res) => {
+  try {
+    const order = await razorpay.orders.create({
+      amount: 100,
+      currency: "INR",
+      receipt: "dns_event_ticket",
+    });
+    res.json(order);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
