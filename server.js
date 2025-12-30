6353d8b6417d9e7a460c2881208b01eac0ff34f7
DIVYANSH327 <143836967+DIVYANSH327@users.noreply.github.com>
2025-12-30 22:10:37 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-app.use("/tickets", express.static("tickets"));
-
-app.get("/", (req, res) => {
-  res.send("DNS Event Backend Live ✅");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
