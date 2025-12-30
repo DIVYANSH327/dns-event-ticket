ccbb7ee8f632d24840159c345ae4d21ae5ecc8a8
DIVYANSH327 <143836967+DIVYANSH327@users.noreply.github.com>
2025-12-30 22:31:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
