86bbf77aaec9794a60539f415b869f86a2cbc3d3
DIVYANSH327 <143836967+DIVYANSH327@users.noreply.github.com>
2025-12-30 22:41:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 require("dotenv").config();
 const express = require("express");
 const Razorpay = require("razorpay");
